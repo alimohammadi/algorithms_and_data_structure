d12384dca3f1454db1399ca14dc1f1611f8e4957
Ali Mohammadi <alimohammadi02321@gmail.com>
2025-02-07 14:31:34 +0330

    Array list

---
+++ arrays/arrayList/src/EmployeeLinkedList.java
@@ -0,0 +1,9 @@
+public class EmployeeLinkedList {
+    private EmployeeNode head;
+
+    public void addToFront(Employee employee){
+        EmployeeNode node = new EmployeeNode(employee);
+        node.setNext(head);
+        head = node;
+    };
+}
